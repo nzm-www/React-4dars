4fbdbd6ea1de5c68ba77446e1d57a611af7f41d2
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-07-22 13:58:20 +0500

    .

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,7 +3,68 @@ import "./header.css";
 import logo from "../../assets/logms.svg";
 import react from "../../assets/react.svg";
 
-export default function header() {
+function header() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [num, setNum] = useState(null);
+  const [count, setCount] = useState([]);
+  const [shahar, setShahar] = useState([]);
+  const [joy, setJoy] = useState("");
+  const [people, setpeople] = useState(null);
+  const [desc, setDesc] = useState();
+
+  // function handleName(event) {
+  //   setName(event.target.value);
+  // }
+  // function handleEmali(event) {
+  //   setEmail(event.target.value);
+  // }
+  // function handleNum(event) {
+  //   setNum(event.target.value);
+  // }
+
+  // function handleCountry(event) {
+  //   setCount(event.target.value);
+  // }
+  // function handleShahar(event) {
+  //   setShahar(event.target.value);
+  // }
+  // function handleJoy(event) {
+  //   setJoy(event.target.value);
+  // }
+
+  // function handlepeople(event) {
+  //   setpeople(event.target.value);
+  // }
+  // function handleDesc(event) {
+  //   setDesc(event.target.value);
+  // }
+
+  function handleForm(event) {
+    event.preventDefault();
+
+    let user = {
+      name,
+      email,
+      num,
+      count,
+      shahar,
+      joy,
+      people,
+      desc,
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+
+    setName("");
+    setEmail("");
+    setNum("");
+    setCount("");
+    setJoy("");
+    setpeople("");
+    setDesc("");
+    setShahar("");
+    console.log(user, "user");
+  }
   return (
     <>
       <div className="header">
@@ -49,18 +110,27 @@ export default function header() {
               <img className="react" src={react} alt="" />
               <p>Yuklash</p>
             </b>
-            <form className="form">
+            <form className="form" onSubmit={handleForm}>
               <div>
                 <h5>Kompaniya nomi *</h5>
                 <input
+                  onChange={(event) => setName(event.target.value)}
                   className="inputss"
                   type="text"
                   placeholder="Kompaniya nomi"
+                  value={name}
                 />
               </div>
               <div>
                 <h5>Email *</h5>
-                <input className="inputss" type="email" placeholder="Email" />
+
+                <input
+                  className="inputss"
+                  type="email"
+                  placeholder="Email"
+                  onChange={(event) => setEmail(event.target.value)}
+                  value={email}
+                />
               </div>
               <div>
                 <h5>Telefon raqami *</h5>
@@ -68,31 +138,37 @@ export default function header() {
                   className="inputss"
                   type="number"
                   placeholder="UZ +9989"
+                  onChange={(event) => setNum(event.target.value)}
+                  value={num}
                 />
               </div>
 
               <h5>Linklar</h5>
               <div className="links">
                 <div className="infos">
-                  <i class="fa-solid fa-earth-americas"></i>
+                  <i className="fa-solid fa-earth-americas"></i>
                 </div>
                 <div className="infos">
-                  <i class="fa-brands fa-instagram"></i>
+                  <i className="fa-brands fa-instagram"></i>
                 </div>
                 <div className="infos">
-                  <i class="fa-brands fa-telegram"></i>
+                  <i className="fa-brands fa-telegram"></i>
                 </div>
                 <div className="infos">
-                  <i class="fa-brands fa-facebook"></i>
+                  <i className="fa-brands fa-facebook"></i>
                 </div>
                 <div className="infos">
-                  <i class="fa-brands fa-github"></i>
+                  <i className="fa-brands fa-github"></i>
                 </div>
               </div>
               <div className="davShah">
                 <span>
                   <h5>Davlat</h5>
-                  <select className="davselec" name="" id="">
+                  <select
+                    className="davselec"
+                    value={count}
+                    onChange={(event) => setCount(event.target.value)}
+                  >
                     <option value="Davlatlar">Davlatlar</option>
                     <option value="Uz">Uz</option>
                     <option value="Ru">Ru</option>
@@ -101,33 +177,50 @@ export default function header() {
                 </span>
                 <span>
                   <h5>Shahar</h5>
-                  <select className="davselec" name="" id="">
+                  <select
+                    className="davselec"
+                    value={shahar}
+                    onChange={(event) => setShahar(value.target.value)}
+                  >
                     <option value="Shahar">Shahar</option>
-                    <option value="Uz">Uz</option>
-                    <option value="Ru">Ru</option>
-                    <option value="Eng">Eng</option>
+                    <option value="Tosh">Tosh</option>
+                    <option value="Far">Far</option>
+                    <option value="Jzx">Jzx</option>
                   </select>
                 </span>
               </div>
               <h5>Yashash joyi *</h5>
               <div>
-                <textarea className="joylashuv" placeholder="Joy"></textarea>
+                <textarea
+                  value={joy}
+                  className="joylashuv"
+                  placeholder="Joy"
+                  onChange={(event) => setJoy(event.target.value)}
+                ></textarea>
               </div>
               <h5>Hodimlar soni *</h5>
               <div>
                 <input
+                  value={people}
                   className="inputss"
                   placeholder="Hodimlar soni"
                   type="number"
+                  onChange={(value) => setpeople(event.target.value)}
                 />
               </div>
               <h5>Izoh</h5>
               <div style={{ paddingBottom: "42px" }}>
-                <textarea className="fikr"></textarea>
+                <textarea
+                  onChange={(value) => setDesc(event.target.value)}
+                  value={desc}
+                  className="fikr"
+                ></textarea>
               </div>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <button className="btnn">Orqaga</button>
-                <button className="btnn">Keyingisi</button>
+                <button className="btnn" type="submit">
+                  Keyingisi
+                </button>
               </div>
             </form>
           </div>
@@ -136,3 +229,4 @@ export default function header() {
     </>
   );
 }
+export default header;
